ba18ef9d2f2eeed5a65daa2f3fe6d0517959a33e
Damion de Wit <damion.dewit@outlook.com>
2017-10-24 11:53:45 +0200

    Started fresh C++ Project

---
+++ Main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+class Rect
+{
+protected:
+	int Width;
+	int Height;
+	int Area;
+public:
+	Rect()
+	{
+		Width = 0;
+		Height = 0;
+		Area = 0;
+	}
+
+	Rect(int width, int height)
+	{
+		Width = width;
+		Height = height;
+		Area = Width * Height;
+	}
+
+	virtual void Print()
+	{
+		std::cout << "Width: " << Width << std::endl;
+		std::cout << "Height: " << Height << std::endl;
+		std::cout << "Area: " << Area << std::endl;
+	}
+};
+
+class Screen : public Rect
+{
+private:
+	const char* Output = "192.168.50.240";
+public:
+	Screen()
+	{
+		Width = 0;
+		Height = 0;
+		Area = 0;
+	}
+
+	Screen(int width, int height)
+	{
+		Width = width;
+		Height = height;
+		Area = Width * Height;
+	}
+
+	void Print() override
+	{
+		std::cout << "Width: " << Width << std::endl;
+		std::cout << "Height: " << Height << std::endl;
+		//std::cout << "Area: " << Area << std::endl;
+		std::cout << "Output: " << Output << std::endl;
+	}
+};
+
+int main()
+{
+	Screen screen{800, 600};
+	screen.Print();
+
+	std::cin.get();
+}
