84ed318bf4658d188cad33f8227a6adfa9d33fed
Damion <damion.dewit@outlook.com>
2017-10-31 15:14:32 +0100

    Added Python script to start omxplayer on the wall Made some to buffer in run_ffmpeg

--- Wall_Stream/src/Main.cpp
+++ Wall_Stream/src/Main.cpp
@@ -1,20 +1,29 @@
+
 #include <iostream>
+#include <windows.h>
 #include <string>
 #include "config.h"
 #include "screen.h"
 
 #define LOG(x) x.Print();
 
+void run_omxplayer()
+{
+	system("python C:\\Users\\Pi\\Source\\Repos\\Wall_Stream\\Wall_Stream/start_omx_on_wall.py");
+	Sleep(1000);
+	
+}
+
 void run_ffmpeg()
 {
-	const std::string &buffer = "ffmpeg -re -i sample.divx -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*0\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.241:1234 -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*0\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.242:1234 -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*0\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.243:1234 -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*1\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.244:1234 -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*1\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.245:1234 -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*1\" -preset ultrafast -vcodec libx264 -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.246:1234";
+	const std::string &buffer = "ffmpeg -i C:\\Users\\Pi\\Videos\\samples\\sample.divx -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*0\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.241:1234 -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*0\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.242:1234 -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*0\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.243:1234 -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*1\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.244:1234 -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*1\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.245:1234 -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*1\" -pixel_format yuv444p -preset default -vcodec h264_nvenc -tune zerolatency -bufsize 30000k -f mpegts udp://192.168.60.246:1234";
 
 	system(buffer.c_str());
 }
 
 int main()
 {
+	run_omxplayer();
 	run_ffmpeg();
-	system("pwd");
 	system("PAUSE");
 }
