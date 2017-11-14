c10e46d96630c9c3c1f8e5bb7d2b1b29251aa884
Damiondewit <damion.dewit@outlook.com>
2017-11-14 12:01:07 +0100

    Currently most stable version developed on Wall Stream PC, some implementation for screencap stream, but still heavily in development

--- Wall_Stream/src/Main.cpp
+++ Wall_Stream/src/Main.cpp
@@ -5,33 +5,55 @@
 
 #define LOG(x) x.Print();
 
-void run_ffmpeg()
+void run_ffmpeg_screenCap()
 {
-	const std::string &input = "4k_sample.mp4";
-	const std::string &preset = "ultrafast";
-	const std::string &vcodec = "libx264";
+	const std::string &input = "-f dshow -i video=\"UScreenCapture\"";
 
-	const std::string iplist[] = {	"192.168.60.241", 
-									"192.168.60.242", 
-									"192.168.60.243", 
+	const std::string iplist[] = { "192.168.60.247"
+	};
+
+	const std::string &buffer = "ffmpeg -hwaccel cuvid -y -rtbufsize 2000M -f gdigrab -framerate 60 -offset_x 0 -offset_y 0 -video_size 4000x2128 -i desktop -c:v h264_nvenc -preset:v hp -pix_fmt yuv444p -profile:v high444p -f mpegts udp://192.168.60.247:1234";
+
+	system(buffer.c_str());
+}
+
+void run_ffmpeg_wall()
+{
+	const std::string &input = "-i C:\\Users\\Pi\\Videos\\samples\\4k_sample.mp4 ";
+	const std::string &preset = "-preset hq ";
+	const std::string &profile = "-profile:v high444p ";
+	const std::string &codec = "-c:v h264_nvenc ";
+	const std::string &hwaccel = "-hwaccel cuvid -c:v h264_cuvid ";
+	const std::string &bufsize = "-bufsize 2000k ";
+	const std::string filter[] = { 
+									" -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*0\" ", 
+									" -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*0\" ",
+									" -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*0\" ",
+									" -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*1\" ",
+									" -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*1\" ",
+									" -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*1\" ",
+									};
+
+	const std::string iplist[] = {	"192.168.60.241",
+									"192.168.60.242",
+									"192.168.60.243",
 									"192.168.60.244",
 									"192.168.60.245",
-									"192.168.60.246" };
+									"192.168.60.246",
+									};
 
-	const std::string &buffer = "ffmpeg -re -i " + input
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*0\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[0] + ":1234"
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*0\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[1] + ":1234"
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*0\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[2] + ":1234"
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*0:ih/2*1\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[3] + ":1234"
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*1:ih/2*1\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[4] + ":1234"
-		+ " -filter:v \"crop=iw/3:ih/2:iw/3*2:ih/2*1\" -preset " + preset + " -vcodec " + vcodec + " -tune zerolatency -bufsize 30000k -f mpegts udp://" + iplist[5] + ":1234";
+	const std::string &buffer = "ffmpeg -re  "   + input
+		+ filter[0] + preset + codec + bufsize
+		+" -f mpegts udp://" + iplist[0] + ":1234"
+		+ filter[1] + preset + codec + bufsize
+		+ " -f mpegts udp://" + iplist[1] + ":1234";
 
 	system(buffer.c_str());
 }
 
 int main()
 {
-	run_ffmpeg();
-	system("pwd");
-	system("PAUSE");
+	system("python start_omx_on_wall.py");
+	run_ffmpeg_wall();
+	//system("PAUSE");
 }
